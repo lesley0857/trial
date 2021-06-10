ecfebb460b8596e8c3a9477943bd18c371bfd3e8
lesley <nwekelesley@gmail.com>
2021-06-10 01:13:54 +0100

    update

--- hosting/admin.py
+++ hosting/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from hosting.models import *
+
 admin.site.register(Checkoutt),
 admin.site.register(Products),
 admin.site.register(Customer),
